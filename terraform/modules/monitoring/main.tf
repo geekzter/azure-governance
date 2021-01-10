cd89a3a375d44999ee2c999e27a0f8fc2d735a0a
Eric van Wijk <eric@van-wijk.com>
2021-01-10 16:26:23 +0100

    Activity Logs to Log Analytics

--- terraform/modules/monitoring/main.tf
+++ terraform/modules/monitoring/main.tf
@@ -1,3 +1,5 @@
+data azurerm_subscription primary {}
+
 resource azurerm_log_analytics_workspace vcd_workspace {
   name                         = "${var.resource_group_name}-loganalytics"
   location                     = var.workspace_location
@@ -6,4 +8,74 @@ resource azurerm_log_analytics_workspace vcd_workspace {
   retention_in_days            = 90 
 
   tags                         = var.tags
+}
+
+resource azurerm_monitor_diagnostic_setting activity_log {
+  name                         = "${var.resource_group_name}-activity-log"
+  target_resource_id           = data.azurerm_subscription.primary.id
+  log_analytics_workspace_id   = azurerm_log_analytics_workspace.vcd_workspace.id
+
+  log {
+    category                   = "Administrative"
+    enabled                    = true
+    retention_policy {
+      enabled                  = false
+    }
+  }
+
+  log {
+    category                   = "Security"
+    enabled                    = true
+    retention_policy {
+      enabled                  = false
+    }
+  }
+
+  log {
+    category                   = "ServiceHealth"
+    enabled                    = true
+    retention_policy {
+      enabled                  = false
+    }
+  }
+
+  log {
+    category                   = "Alert"
+    enabled                    = true
+    retention_policy {
+      enabled                  = false
+    }
+  }
+
+  log {
+    category                   = "Recommendation"
+    enabled                    = true
+    retention_policy {
+      enabled                  = false
+    }
+  }
+
+  log {
+    category                   = "Policy"
+    enabled                    = true
+    retention_policy {
+      enabled                  = false
+    }
+  }
+
+  log {
+    category                   = "Autoscale"
+    enabled                    = true
+    retention_policy {
+      enabled                  = false
+    }
+  }
+
+  log {
+    category                   = "ResourceHealth"
+    enabled                    = true
+    retention_policy {
+      enabled                  = false
+    }
+  }
 }
